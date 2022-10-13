56d4bdaa61f3b90094631853c4a41d6b2175a66f
andrijant <andrija_ds@yahoo.com>
2022-10-13 14:43:43 +0200

    java methods

---
+++ src/javaapplication12/Test12.java
@@ -0,0 +1,42 @@
+package javaapplication12;
+
+import java.util.Scanner;
+
+public class Test12 {   // telo klase
+                        // metod mora biti unutar tela klase 
+                        // ...ali izvan tela drugih metoda
+    public static void main(String[] args) {  // telo metoda  
+        //  public - znaci da je taj metod dostupan iz spolja sveta 'public class Test12'
+        //  static - taj metod je dostupan bez potrebe da napravimo instancu klase, objekat
+        //  void - (specijalna rez. rec)znaci da metod 'main' ne da vrati podatke onome ko ga je pozvao,
+        //  ...ne vraca nikakvu vrednost kao rezultat svog izvrsavanja  
+        //  main - ime
+        
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int firstInt = s.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int secondInt = s.nextInt();
+        
+        int smallerThenTwoNum = minimumTwoNumbers(firstInt, secondInt);
+        
+
+        System.out.println("Smaller number is: " + smallerThenTwoNum);
+    }
+    
+    public static int minimumTwoNumbers( int a, int b ) {    // telo metoda 'minimumTwoNumbers'
+        // metod u zagradama '()' uzima argumente koji moraju da imaju tip moze i vise njih
+        // tip podataka 'int'
+        // lokalno ime 'a','b'...
+        // ovaj metod vraca jedan podatak tipa 'int'
+        
+        if ( a < b ) { // naredbe koje se izvrsavaju unutar metoda
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
+
